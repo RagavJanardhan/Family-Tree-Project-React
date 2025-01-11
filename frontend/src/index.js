b49f73b04a5ce83247ba534a0d491544812ca9b6
Ragav Janardhan <23janardhanragav@gmail.com>
2025-01-11 06:31:45 -0500

    Add HomePage component and styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,12 @@
+// frontend/src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './App';  // Import the App component
+import './index.css';    // Optional: Import styles
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')  // This matches the element in your public/index.html
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
