e1f81aaac65ae9daf9c1f5d80594da496abe9512
Ragav Janardhan <23janardhanragav@gmail.com>
2025-01-14 01:12:37 -0500

    data after changes can be downloaded

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,29 @@
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
-import os
 
 app = Flask(__name__, static_folder="frontend/build")
 CORS(app)  # Allow cross-origin requests from React
 
-# API endpoint for testing communication between React and Flask
-@app.route('/api')
-def api():
-    return jsonify({"message": "Hello from Flask!"})
+# Predefined family tree data
+family_data = [
+    {
+        "id": "0",
+        "rels": {},
+        "data": {
+            "first name": "Ragav",
+            "last name": "Janardhan",
+            "birthday": "2005-12-02",
+            "avatar": "https://static8.depositphotos.com/1009634/988/v/950/depositphotos_9883921-stock-illustration-no-user-profile-picture.jpg",
+            "gender": "M",
+            "death date": ""  
+        }
+    }
+]
+
+# Endpoint to serve family tree data
+@app.route('/family-data', methods=['GET'])
+def get_family_data():
+    return jsonify(family_data)
 
 # Serving React App (index.html)
 @app.route('/')
