77a3a9514183223d80b98b945d2eb79cc96a5346
Ragav Janardhan <23janardhanragav@gmail.com>
2025-01-20 02:00:07 -0500

    More UI changes

--- frontend/src/components/FamilyTree/FamilyTreePage.js
+++ frontend/src/components/FamilyTree/FamilyTreePage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useTheme } from '@mui/material/styles';
 import FamilyTree from './FamilyTree'; // Import the FamilyTree component
 
 const FamilyTreePage = () => {
+  const theme = useTheme(); // Access the current theme
+
   return (
-    <div className="family-tree-page" style={{ height: '100%' }}>      {/* Family Tree Page Title */}
+    <div className="family-tree-page" 
+    style={{
+      height: '100%',
+      '--primary-main': theme.palette.primary.main, // Inject primary.main as a CSS variable
+      '--text-primary': theme.palette.text.primary,
+      '--text-secondary': theme.palette.text.secondary, // Inject primary.main as a CSS variable
+    }}
+    >      
+      {/* Family Tree Page Title */}
       <h3>
         Family Tree Visualization
       </h3>
@@ -15,7 +26,7 @@ const FamilyTreePage = () => {
 
       {/* Family Tree component */}
       <div style={{ width: '100%', margin: '0 auto' }}>
-        <FamilyTree />
+        <FamilyTree theme={theme}/>
       </div>
     </div>
   );
